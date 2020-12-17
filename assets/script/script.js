8c31a7652bfccec6cf6e6a1c9eddef70b9ff3c7a
Wendy Fgigueroa <camomila444@gmail>
2020-12-17 11:13:08 -0500

    updating

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,11 +1,14 @@
 $(document).ready(function () {
 
-  var citySearch;
-  var APIkey = '14d2382c0b1c2cee7fb864ef665df929';
-  var weatherAPI = 'https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}';
-  var forecastAPI = 'https://api.openweathermap.org/data/2.5/forecast/daily?q={city name}&cnt={cnt}&appid={API key}';
-  var uviAPI = 'http://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid={API key}';
 
+  var APIkey = '14d2382c0b1c2cee7fb864ef665df929';
+  var weatherAPI = 'https://api.openweathermap.org/data/2.5/weather?';
+  var forecastAPI = 'https://api.openweathermap.org/data/2.5/forecast?q=';
+  var uviAPI = 'https://api.openweathermap.org/data/2.5/uvi?lat=';
+  var citySearch;
+  var CurrentForecast;
+  var currentLoc;
+  
 
 
   $("#searchButton").on("click", function () {
@@ -64,6 +67,8 @@ $(document).ready(function () {
 
     $("#city-name").empty();
 
+  
+
     //get and set content 
     card = $("<div>").addClass("card");
     cardBody = $("<div>").addClass("card-body");
@@ -72,8 +77,6 @@ $(document).ready(function () {
     huminity = $("<p>").addClass("card-text current-huminity").text("huminity: " + response.main.humidity + "%");
     wind = $("<p>").addClass("card-text current-wind").text("wind speed: " + response.wind.speed + "MPH");
     currentPic = $("<img>").attr("src", "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png")
-
-
 
     // insert to page 
     city.append(currentPic)
@@ -86,22 +89,92 @@ $(document).ready(function () {
 
 
   function getCurrentForecast(city) {
-    console.log("letter", city)
+  
     var forecastAPI = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + APIkey;
     $.ajax({
       url: forecastAPI,
       method: "GET"
-
     })
 
       .then(function (response) {
-
         console.log(response);
-
       });
   }
 
 
+
+//get UV 
+var uvURL = 'https://api.openweathermap.org/data/2.5/uvi?appid=14d2382c0b1c2cee7fb864ef665df929&lat=' + response.coord.lat + '&lon='  + response.coord.lon;
+$.ajax({ 
+  url: uvURL,
+  method: "GET"
+}).then(function (uvresponse) { 
+  var uvindex = uvresponse.value;
+  var bgcolor;
+  if (uvindex <= 3) {
+      bgcolor = "green";
+  }
+  else if (uvindex >= 3 || uvindex <= 6) {
+      bgcolor = "yellow";
+  }
+  else if (uvindex >= 6 || uvindex <= 8) {
+      bgcolor = "orange";
+  }
+  else {
+      bgcolor = "red";
+  }
+  var uvdisp = $("<p>").attr("class", "card-text").text("V Index:");
+  uvdisp.append($("span>").attr("class" , "uvindex").attr("style" , ("background-color:" + bgcolor)).text(uvindex));
+  cardBody.append(uvdisp);
+
+
+});
+
+
+getFiveDayForecast();
+  function getFiveDayForecast() {
+      cardrow.empty();
+      var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + APIkey;
+      $.ajax({  
+        url: queryURL,  
+        method: "GET"
+         
+       })
+      .then(function(fiveDayResponse) {  
+        for (let i = 0; i != fiveDayResponse.list.length; i+=8 ) {
+            let cityObj = {
+                date: fiveDayResponse.list[i].dt_txt,
+                icon: fiveDayResponse.list[i].weather[0].icon,
+                temp: fiveDayResponse.list[i].main.temp,
+            }
+            let dateStr = cityobj.date;
+            let trimmedDate = dateStr.substring(0, 10);
+            let weatherIco = 'https://openweathermap.org/img/w/+ cityObj.icon + png'
+            createForecastCard(trimmedDate, weatherIco , cityObj.temp, cityObj.huminity)
+        }
+    
+      })
+
+  }
+     
+
+
+
+
+//   function getCurrentLocation(position) {  
+//   var lon = position.coords.longitude;
+//   var lat = position.coords.latitude;
+//   var queryURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&APPID=14d2382c0b1c2cee7fb864ef665df929"; 
+//   $.ajax({ 
+//        url: queryURL, 
+//        method: "GET"
+//   }).then(function (response) {
+//     currentLoc = response.name;
+//     saveLoc(esponse.name);
+//     getCurrent(currentLoc);
+//   });
+
+//  } 
 
 
 
